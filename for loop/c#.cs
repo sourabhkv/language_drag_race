54e0cbc9ce746d677c780145b2f3e6fc67f6d648
Sourabh kumar verma <55890376+sourabhkv@users.noreply.github.com>
2022-07-16 21:58:52 +0530

    Create c#.cs

---
+++ for loop/c#.cs
@@ -0,0 +1,14 @@
+using System;
+namespace test
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (int i=0;i<1000000;i++)
+            {
+                continue;
+            }
+        }
+    }
+}
