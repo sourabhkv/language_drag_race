8d0b2282afa1fd9fe693e7756453b88f9d76fcf1
Sourabh kumar verma <55890376+sourabhkv@users.noreply.github.com>
2022-07-22 20:39:55 +0530

    Update c.c

--- sum/c.c
+++ sum/c.c
@@ -1,9 +1,8 @@
-#include <iostream>
-int main() {
-    long long int sum=0;
-    for (int i=1;i<1000001;i++){
-        sum=sum+i;
-    }
-    std::cout<<sum;
-    return 0;
+#include <stdio.h>
+void main(){
+	long long int sum=0,i;
+	for (i=1;i<1000001;i++){
+		sum=sum+i;
+	}
+	printf("%lld",sum);
 }
