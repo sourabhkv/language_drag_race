6701a10e823b1cf72172d5ed7c503729065eb2c7
Sourabh kumar verma <55890376+sourabhkv@users.noreply.github.com>
2022-07-24 20:38:51 +0530

    Update c++.cpp

--- for loop/c++.cpp
+++ for loop/c++.cpp
@@ -1,6 +1,8 @@
 int main() {
-    for (int i=0;i<1000000;i++){
-        continue;
-    }
-    return 0;
+	for (int i=0;i<1000;i++){
+		for (int j=0;j<1000;j++){
+			continue;
+		}
+	}
+	return 0;
 }
